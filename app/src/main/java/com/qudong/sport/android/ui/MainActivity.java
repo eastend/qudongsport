927559dbb2874a33fd40f8fc410a3abe69a6af9f
eastend <798302557@qq.com>
2018-07-22 20:44:00 +0800

    commit

--- app/src/main/java/com/qudong/sport/android/ui/MainActivity.java
+++ app/src/main/java/com/qudong/sport/android/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.qudong.sport.android.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.qudong.sport.android.R;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Toast.makeText()
 
 
 
